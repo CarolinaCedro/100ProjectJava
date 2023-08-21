93c5fe3b50323e662db5ffd63cee8162e8ec3d51
CarolinaCedro <yotechgirl@gmail.com>
2023-08-21 20:47:32 -0300

    ++

--- src/Demo.java
+++ src/Demo.java
@@ -3,9 +3,22 @@ Simulador de Caixa Eletrônico: Crie um programa que simule um caixa eletrônico
  permitindo aos usuários verificar saldo, sacar dinheiro e depositar dinheiro.
  */
 
+import domain.Conta;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
 
     public static void main(String[] args) {
+
+        List<Conta> clientes = new ArrayList<>();
+
+
+
         System.out.println("hello");
     }
+
+
+
 }
